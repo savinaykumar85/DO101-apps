2c04630698396df21217c5a747a858c8b747ce55
openshift user <openshift@gmail.com>
2020-05-21 01:04:57 +0530

    final commit

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -4,8 +4,12 @@ app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
+app.get('/Mars', function (req, res) {
+  res.send('Hello Mars!\n');
+});
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+
+app.listen(8090, function () {
+  console.log('Example app listening on port 8090!');
 });
 
